a214241662106cf003650ec5ae3c1c91d2421249
Koos van Wijk <koosvanw@gmail.com>
2025-09-19 14:36:23 +0200

    Make these actions work for internal usage
    * Use internal repo for the actions
    * Correct parsing of input variables
    * Wait longer between retries
    * Limit audit log size to last 2 days

--- src/audit.js
+++ src/audit.js
@@ -4,9 +4,21 @@ const { createGitHubAppClient } = require('../lib/app');
 
 const main = async () => {
   await utils.registerSecrets()
+  
+  // Validate required environment variables
+  if (!process.env.GH_APP_ID) {
+    throw new Error('GH_APP_ID environment variable is required')
+  }
+  if (!process.env.GH_APP_PRIVATE_KEY) {
+    throw new Error('GH_APP_PRIVATE_KEY environment variable is required')
+  }
+  if (!process.env.GH_APP_INSTALLATION_ID) {
+    throw new Error('GH_APP_INSTALLATION_ID environment variable is required')
+  }
+  
   const appClient = await createGitHubAppClient()
   const client = await appClient.getInstallationOctokit(process.env.GH_APP_INSTALLATION_ID)
-  const since = await utils.getSince(7)
+  const since = await utils.getSince(2)
   await client.inactiveUsers.audit(process.env.ORG, since)
 }
 
